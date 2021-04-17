6f866cd52bf50b378fb57c19f273fa4afd31ef25
OxyEho <valera808@inbox.ru>
2021-04-17 19:30:13 +0500

    fix names parsing

--- client/__main__.py
+++ client/__main__.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-f', type=str, default='<>')
     arg_parser.add_argument('-t', '--to', type=str)
-    arg_parser.add_argument('-d', '--directory', type=str)
+    arg_parser.add_argument('-d', '--directory', type=str, default='.')
     arg_parser.add_argument('--subject', type=str, default='Happy pictures')
     arg_parser.add_argument('-s', '--server', type=str, default='smtp.mail.ru:25')
     arg_parser.add_argument('--ssl', action='store_true')
--- client/file.py
+++ client/file.py
@@ -4,7 +4,10 @@ import base64
 class File:
     def __init__(self, file_name: str):
         self.full_file_name = file_name
-        self.file_name = file_name.split('/')[-1]
+        if '\\' in file_name:
+            self.file_name = file_name.split('\\')[-1]
+        elif '/' in file_name:
+            self.file_name = file_name.split('/')[-1]
         self.base64_name = base64.b64encode(self.file_name.encode('utf-8')).decode('utf-8')
 
     def get_base64(self) -> str:
